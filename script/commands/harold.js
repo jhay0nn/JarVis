9b0f7439f3aba1fd1a96db857e5f723e1a27ed70
jhay0nn <151178650+jhay0nn@users.noreply.github.com>
2024-04-15 20:53:20 +0800

    Update harold.js

--- script/commands/harold.js
+++ script/commands/harold.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 
 module.exports.config = {
-    name: "harold",
+    name: "jarvis",
     version: "1.0.0",
     hasPermssion: 0,
     credits: "Jonell Magallanes",
