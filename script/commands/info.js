88b0fa30af8e3e175f1b67b7ec328b3923e70590
jhay0nn <151178650+jhay0nn@users.noreply.github.com>
2024-04-15 22:22:21 +0800

    Update info.js

--- script/commands/info.js
+++ script/commands/info.js
@@ -66,7 +66,7 @@ module.exports.run = async function ({ api, event, args, Users, permssion, getTe
       "MintBot có thể hiểu phụ nữ nhưng không thể có được họ.",
       "MintBot cân spam nhưng không có gì đáng để bạn spam."
     ];
-    var link = ["https://ibb.co/1n2j79N"];
+    var link = ["https://i.postimg.cc/sx7pbJKv/379247190-122110282928035450-461131913890350154-n.jpg"];
 
     var i = 1;
     var msg = [];
